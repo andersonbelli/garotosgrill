c49937423e186ddaf2b2dfbfe993cdd2ae9f38ca
Anderson Belli <anderson.belli@vortigo.com.br>
2022-04-20 15:56:35 -0500

    Changing banner images and js

--- js/main.js
+++ js/main.js
@@ -1,27 +1,27 @@
 
-// (function ($) {
-//     "use strict";
-//     /*==================================================================
-//     [ Simple slide100 ]*/
+(function ($) {
+    "use strict";
+    /*==================================================================
+    [ Simple slide100 ]*/
 
-//     $('.simpleslide100').each(function(){
-//         var delay = 7000;
-//         var speed = 2000;
-//         var itemSlide = $(this).find('.simpleslide100-item');
-//         var nowSlide = 0;
+    $('.simpleslide100').each(function(){
+        var delay = 7000;
+        var speed = 2000;
+        var itemSlide = $(this).find('.simpleslide100-item');
+        var nowSlide = 0;
 
-//         $(itemSlide).hide();
-//         $(itemSlide[nowSlide]).show();
-//         nowSlide++;
-//         if(nowSlide >= itemSlide.length) {nowSlide = 0;}
+        $(itemSlide).hide();
+        $(itemSlide[nowSlide]).show();
+        nowSlide++;
+        if(nowSlide >= itemSlide.length) {nowSlide = 0;}
 
-//         setInterval(function(){
-//             $(itemSlide).fadeOut(speed);
-//             $(itemSlide[nowSlide]).fadeIn(speed);
-//             nowSlide++;
-//             if(nowSlide >= itemSlide.length) {nowSlide = 0;}
-//         },delay);
-//     });
+        setInterval(function(){
+            $(itemSlide).fadeOut(speed);
+            $(itemSlide[nowSlide]).fadeIn(speed);
+            nowSlide++;
+            if(nowSlide >= itemSlide.length) {nowSlide = 0;}
+        },delay);
+    });
 
 
-// })(jQuery);
+})(jQuery);
